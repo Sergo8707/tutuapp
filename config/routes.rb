3c83b344267032fbd18f8453f9565dc6563dbaf8
Sergo8707 <sergo8707@gmail.com>
2017-02-05 23:37:08 +0200

    fix bugs 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   resources :routes
   resources :carriages
 
-  resources :cupe_carriage, controller: 'carriages', type: 'CupeCarriage'
-  resources :platc_carriage, controller: 'carriages', type: 'PlatcCarriage'
-  resources :seats_carriage, controller: 'carriages', type: 'SeatsCarriage'
+  resources :cupe_carriages, controller: 'carriages', type: 'CupeCarriage'
+  resources :platc_carriages, controller: 'carriages', type: 'PlatcCarriage'
+  resources :seats_carriages, controller: 'carriages', type: 'SeatsCarriage'
   resources :upholstered_carriages, controller: 'carriages', type: 'UpholsteredCarriage'
 
   get 'welcome/index'
