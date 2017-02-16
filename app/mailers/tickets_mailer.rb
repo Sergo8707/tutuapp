9c3b6bfb452b8dcffd216a6dbc1a56d922c2bac6
Sergo8707 <sergo8707@gmail.com>
2017-02-16 19:48:34 +0200

    add mailer

---
+++ app/mailers/tickets_mailer.rb
@@ -0,0 +1,15 @@
+class TicketsMailer < ApplicationMailer
+  def buy_ticket(user, ticket)
+    @user = user
+    @ticket = ticket
+    mail(to: user.email, subject: 'Вы купили билет')
+  end
+
+  def delete_ticket(user, ticket)
+    @user = user
+    @ticket = ticket
+    mail(to: user.email, subject: 'Вы удалили билет')
+  end
+
+
+end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -10,10 +10,27 @@ class Ticket < ApplicationRecord
   validates :document, presence: true
   validates :start_station, :end_station, presence: true
 
+  after_create :send_notification
+  before_destroy :send_ticket_delete
+
+  def route_name
+    "#{start_station.name} - #{end_station.name}"
+  end
+
   class << self
     def new_number
       current_max = maximum(:number).to_i || 0
       current_max += 1
     end
+  end
+
+  private
+
+  def send_notification
+    TicketsMailer.buy_ticket(self.user, self).deliver_now
+  end
+
+  def send_ticket_delete
+    TicketsMailer.delete_ticket(self.user, self).deliver_now
   end
 end
