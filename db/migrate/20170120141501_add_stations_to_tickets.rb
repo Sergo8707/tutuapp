e33176bcb5a99c9edcdd49c1416de1cb6e253eb4
Sergo8707 <sergo8707@gmail.com>
2017-02-17 16:56:41 +0200

    fix5

--- db/migrate/20170120141501_add_stations_to_tickets.rb
+++
@@ -1,8 +0,0 @@
-class AddStationsToTickets < ActiveRecord::Migration[5.0]
-  def change
-
-    add_belongs_to :tickets, :start_station, foreign_key: true
-    add_belongs_to :tickets, :end_station, foreign_key: true
-
-  end
-end
