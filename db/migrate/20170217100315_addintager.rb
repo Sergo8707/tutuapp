ad6515d836e2c2c97883b85979cbc63f9816546b
Sergo8707 <sergo8707@gmail.com>
2017-02-17 17:50:37 +0200

    fix 11

--- db/migrate/20170217100315_addintager.rb
+++
@@ -1,5 +0,0 @@
-class Addintager < ActiveRecord::Migration[5.0]
-  def change
-    change_column :carriages, :number, :integer
-  end
-end
--- db/migrate/20170217140140_remote_statr_station.rb
+++
@@ -1,6 +0,0 @@
-class RemoteStatrStation < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :tickets, :start_station_id, :integer, null: false
-    remove_column :tickets, :end_station_id, :integer, null: false
-  end
-end
