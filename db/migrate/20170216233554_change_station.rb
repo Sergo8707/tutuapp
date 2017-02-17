aacd3fde9b3ad36aa3d277decfab59f06ddcab01
Sergo8707 <sergo8707@gmail.com>
2017-02-17 17:47:56 +0200

    fix 10

--- db/migrate/20170216233554_change_station.rb
+++
@@ -1,6 +0,0 @@
-class ChangeStation < ActiveRecord::Migration[5.0]
-  def change
-    change_column :tickets, :start_station_id, :integer
-    change_column :tickets, :end_station_id, :integer
-  end
-end
