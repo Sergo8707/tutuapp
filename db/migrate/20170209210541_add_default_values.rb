042f64e3760682c91efaf04b0579aa39b31b312a
Sergo8707 <sergo8707@gmail.com>
2017-02-17 17:43:10 +0200

    fix 8

--- db/migrate/20170209210541_add_default_values.rb
+++
@@ -1,8 +0,0 @@
-class AddDefaultValues < ActiveRecord::Migration[5.0]
-  def change
-    change_column :tickets, :train_id, :integer, null: false
-    change_column :tickets, :user_id, :integer, null: false
-    change_column :tickets, :start_station_id, :integer, null: false
-    change_column :tickets, :end_station_id, :integer, null: false
-  end
-end
